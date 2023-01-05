e89a1653b1b4a9cc7a8bd6bd297275865478f067
ettoreciarcia <ettoreciarcia.tech@gmail.com>
2023-01-05 18:26:54 +0100

    Added s3 bucket polixy

--- modules/infra/main.tf
+++ modules/infra/main.tf
@@ -25,6 +25,25 @@ resource "aws_s3_bucket_website_configuration" "bucket_website" {
   }
 }
 
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.website.id
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::${aws_s3_bucket.website.bucket}/*"
+        }
+    ]
+}
+POLICY
+}
+
 resource "aws_cloudfront_distribution" "static_website" {
   enabled = true
   origin {
